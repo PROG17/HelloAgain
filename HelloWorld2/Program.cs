f9cac73b4ed21c586ed29fc7f5390a885136b079
Egidio Ricchiuti <egisan3@yahoo.com>
2017-09-17 23:27:26 +0200

    Egidio added a text

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld2
         static void Main(string[] args)
         {
             Console.WriteLine("Fredrik was here!?!?!");
+            Console.WriteLine("Egidio tesing");
         }
     }
 }
